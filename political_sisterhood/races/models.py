473d355d1fdda6e3859481887503c4b9e890a7a8
cmallon <christopher.mallon@gmail.com>
2018-03-29 15:35:03 -0400

    Fix extensions and change race str

--- political_sisterhood/races/models.py
+++ political_sisterhood/races/models.py
@@ -51,13 +51,19 @@ class State(models.Model):
 class Race(models.Model):
     state = models.ForeignKey(State, on_delete=models.CASCADE, related_name="states")
     district = models.CharField(max_length=255, blank=True)
+    other = models.CharField(max_length=255, blank=True, help_text="If there is another descriptor for the race")
     RACE = Choices('Senate', 'House')
     race_type = StatusField(choices_name='RACE')
     filing_date = models.DateField(blank=True, null=True)
     open_race = models.BooleanField(default=False)
 
     def __str__(self):
-        return "{} - {}".format(self.district, self.state)
+        if self.district:
+            return "District {} - {}".format(self.district, self.state)
+        elif self.other:
+            return "{} - {}".format(self.other, self.state)
+        else:
+            return self.state
 
 
 class RaceEntry(models.Model):
