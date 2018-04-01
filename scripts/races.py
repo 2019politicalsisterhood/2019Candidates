75f6f54d3b7fd695ba4e72da2b364d89fe512b52
cmallon <christopher.mallon@gmail.com>
2018-04-01 14:50:27 -0400

    Don't bring in nan

--- scripts/races.py
+++ scripts/races.py
@@ -8,16 +8,16 @@ logger = logging.getLogger(__name__)
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def run():
-    data = pd.read_csv(os.path.join(ROOT_DIR, 'race.csv'))
+    data = pd.read_csv(os.path.join(ROOT_DIR, 'race.csv'), keep_default_na=False)
     df = pd.DataFrame(data)
     for index, row in df.iterrows():
         try:
             state = row[0]
-            name = row[1]
+            name = str(row[1])
             race = row[2]
             filing = row[3]
             other = row[4]
             state = State.objects.get(state=state)
-            Race.objects.get_or_create(state=state, district=name, race_type=race, filing_date=filing, other=other)
+            Race.objects.get_or_create(state=state, district=name, race_type=race, other=other)
         except Exception as e:
-            logger.warning(e)
+            logger.warning(e, exc_info=True)
