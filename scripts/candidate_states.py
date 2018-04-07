31583fab253bf74b66e4c3b25cf7829be6d89d81
cmallon <christopher.mallon@gmail.com>
2018-04-07 10:20:49 -0400

    Addidng states update work

---
+++ scripts/candidate_states.py
@@ -0,0 +1,23 @@
+from political_sisterhood.candidate.models import Candidate
+import pandas as pd
+import os
+import logging
+import csv
+logger = logging.getLogger(__name__)
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def run():
+    data = pd.read_csv(os.path.join(ROOT_DIR, 'candidate_state.csv'), keep_default_na=False)
+    df = pd.DataFrame(data)
+    for index, row in df.iterrows():
+        try:
+            first = row[0]
+            last = row[1]
+            state = row[2]
+            candidate = Candidate.objects.filter(first_name=first, last_name=last)
+            if not candidate:
+                logger.info(first + " " +last)
+            candidate.update(state=state)
+        except Exception as e:
+            logger.warning(e, exc_info=True)
