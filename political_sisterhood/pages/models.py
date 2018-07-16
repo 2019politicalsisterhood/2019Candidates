fc19070f1ac6f9e9010fe08908aeb33e25f325ff
cmallon <christopher.mallon@gmail.com>
2018-07-15 20:58:13 -0400

    Change name on pages

--- political_sisterhood/pages/models.py
+++ political_sisterhood/pages/models.py
@@ -7,3 +7,6 @@ class Page(models.Model):
     title = models.CharField(max_length=255)
     content = RichTextField()
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
