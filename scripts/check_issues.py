81370a099f51898dde2235052ef30609ad98958d
cmallon <christopher.mallon@gmail.com>
2018-07-15 21:52:47 -0400

    Stupid gitignore

---
+++ scripts/check_issues.py
@@ -0,0 +1,23 @@
+from political_sisterhood.candidate.models import Candidate
+from django.db.models import Q
+from templated_email import send_templated_mail
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def run():
+    candidate = Candidate.objects.filter(Q(issue1__regex='[a-zA-Z ]'),
+                                         Q(issue2__regex='[a-zA-Z ]'),
+                                         Q(issue3__regex='[a-zA-Z ]'))
+    try:
+        send_templated_mail(
+            template_name='issueWithIssues',
+            from_email="Political Sisterhood <info@politicalsisterhood.org>",
+            recipient_list=['chris@politicalsisterhood.com'],
+            context={
+                'candidates': candidate,
+            }
+        )
+    except Exception as e:
+        logger.error(e)
